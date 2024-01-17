041c690c8c57ab3da71aa36aa1a2a36ac4f9f710
NoumaanAhamed <noumaanahamed@gmail.com>
2024-01-18 02:51:32 +0530

    added more support

--- index.js
+++ index.js
@@ -11,19 +11,62 @@ const webhookUrl = "http://localhost:3000/webhook";
 const web3 = new Web3(new Web3.providers.WebsocketProvider(providerUrl));
 const contract = new web3.eth.Contract(abi, contractAddress);
 
-// async function main() {
-//     const accounts = await web3.eth.getAccounts();
-//     console.log(accounts);
-// }
+const fromAddress = '0xd68c62F898371Cd602d639eC190A70C6F0101d7f';
+const toAddress = '0xA5D19Db5428ab1eEe51D4047b4aE31E83fba1c5F';
 
-contract.events.Transfer({
-    fromBlock: 0
-})
-.on('data', async function(event) {
-    console.log(`Received ${event.returnValues.value}`);
-    // await axios.post(webhookUrl, {
-    //     value: event.returnValues.value.toString()
-    // });
-})
+
+
+function viewAllTranscations(){
+    contract.events.Transfer({
+        fromBlock: 0
+    })
+    .on('data', async function(event) {
+        console.log(`Received ${event.returnValues.value}`);
+        // await axios.post(webhookUrl, {
+        //     value: event.returnValues.value.toString()
+        // });
+    })
+
+}
+
+function viewBalance(fromAddress){
+    contract.methods.balanceOf(fromAddress).call().then((balance) => {
+        console.log(`Balance of ${fromAddress} is ${balance}`);
+    })
+}
+
+function viewTranscationsOfSender(targetWalletAddress){
+    contract.events.Transfer({
+        fromBlock: 0,
+        filter: { from: targetWalletAddress } // Specify the address you want to filter by
+    })
+    .on('data', async function(event) {
+        console.log(`Received ${event.returnValues.value} for address ${event.returnValues.to}`);
+    })
+    
+}
+
+function viewTranscationsOfReceiver(targetWalletAddress){
+    contract.events.Transfer({
+        fromBlock: 0,
+        filter: { to: targetWalletAddress } // Specify the address you want to filter by
+    })
+    .on('data', async function(event) {
+        console.log(`Received ${event.returnValues.value} from address ${event.returnValues.from}`);
+    })
+    
+}
+
+function main(){
+    viewBalance(fromAddress);
+    viewTranscationsOfSender(fromAddress);
+    viewTranscationsOfReceiver(toAddress);
+}
+
+
+
+// viewBalance(fromAddress);
+// viewAllTranscations();
+
 
 // main();
